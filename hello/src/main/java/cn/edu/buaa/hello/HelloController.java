e319449ebcee04fa0c6b6c5d5a67604398fea199
LinkChain <wangrong_cool@sina.com.cn>
2017-09-17 20:16:35 +0800

    Update HelloController.java

--- hello/src/main/java/cn/edu/buaa/hello/HelloController.java
+++ hello/src/main/java/cn/edu/buaa/hello/HelloController.java
@@ -3,13 +3,7 @@ package cn.edu.buaa.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Spring Boot HelloWord
- * ClassName: HelloController 
- * @Description: TODO
- * @author wangrong
- * @date 2017年9月16日
- */
+
 @RestController
 public class HelloController {
 
